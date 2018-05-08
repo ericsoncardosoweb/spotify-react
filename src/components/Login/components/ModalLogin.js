97d009bd402089ffec4a017fec2771985a724f9b
Ericson Cardoso <ericson.cardoso@bliteti.com.br>
2018-05-07 22:49:14 -0300

    buttons - modal login

--- src/components/Login/components/ModalLogin.js
+++ src/components/Login/components/ModalLogin.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './style.scss'
 
 const ModalLogin = ({ userAuth }) => (
   <div id="modal-login" className="modal">
-    <h2 className="modal__title">Bem Vindo!</h2>
-    <p>Clique no botão a baixo para acessar sua conta.</p>
-    <button className='button primary block' onClick={
-      console.log('iniciar login')
-    }>Acessar minha conta</button>
+    <div className="modal__content">
+      <h2 className="modal__title">Bem Vindo!</h2>
+      <p>Clique no botão a baixo para acessar sua conta.</p>
+      <button className='button flat primary block' onClick={
+        console.log('iniciar login')
+      }>Acessar minha conta</button>
+    </div>
   </div>
 )
 
